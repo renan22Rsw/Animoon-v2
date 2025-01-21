f77ee477e785b50784784d38a68dfbf7d0e8474a
Renan Victor <renan22rsw@gmail.com>
2025-01-21 01:11:06 -0300

    feat: Info Columns component

---
+++ src/components/InfoColumns/info-columns.tsx
@@ -0,0 +1,33 @@
+import { columnsInfos } from "./infos";
+
+const InfoColumns = () => {
+  return (
+    <div className="hidden justify-end bg-yellow-400 sm:flex xl:w-1/4 2xl:w-5/12">
+      <div className="m-2 w-[160px] rounded-md bg-zinc-400 p-2">
+        <div className="space-y-2 text-xs">
+          {columnsInfos.map((item) => (
+            <div key={item.id}>
+              <h5 className="font-bold">{item.title}</h5>
+              <p className="font-thin">{item.info}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InfoColumns;
+
+<div className="hidden justify-end bg-yellow-400 sm:flex xl:w-[320px] 2xl:w-[780px]">
+  <div className="m-2 w-[160px] rounded-md bg-zinc-400 p-2">
+    <div className="space-y-2 text-xs">
+      {columnsInfos.map((item) => (
+        <div key={item.id}>
+          <h5 className="font-bold">{item.title}</h5>
+          <p className="font-thin">{item.info}</p>
+        </div>
+      ))}
+    </div>
+  </div>
+</div>;
---
+++ src/components/InfoColumns/infos.ts
@@ -0,0 +1,53 @@
+interface Infos {
+  id: number;
+  title: string;
+  info: string;
+}
+
+export const columnsInfos: Infos[] = [
+  {
+    id: 1,
+    title: "Format",
+    info: "TV",
+  },
+  {
+    id: 2,
+    title: "Episode Duration",
+    info: "23min",
+  },
+  {
+    id: 3,
+    title: "Status",
+    info: "Finhished",
+  },
+
+  {
+    id: 4,
+    title: "Start Date",
+    info: "Apr 1, 2024",
+  },
+
+  {
+    id: 5,
+    title: "End Date",
+    info: "Apr 1, 2024",
+  },
+
+  {
+    id: 6,
+    title: "Season",
+    info: "Winter 2023",
+  },
+
+  {
+    id: 7,
+    title: "Avarage Score",
+    info: "91%",
+  },
+
+  {
+    id: 8,
+    title: "Mean Score",
+    info: "92%",
+  },
+];
