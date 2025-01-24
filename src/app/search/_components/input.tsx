31fad7917d86186ad9be99d2ddc0b8ccfe85b7f3
Renan Victor <renan22rsw@gmail.com>
2025-01-24 12:20:56 -0300

    feat: search for contents function

--- src/app/search/_components/input.tsx
+++ src/app/search/_components/input.tsx
@@ -1,8 +1,40 @@
+"use client";
+
 import { Input } from "@/components/ui/input";
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
-import SelectGenres from "./select";
+import SelectGenre from "./select";
+import { useEffect, useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
 
 const InputSearch = () => {
+  const [search, setSearch] = useState<string>("");
+  const [genre, setGenre] = useState<string>("");
+
+  const router = useRouter();
+  const pathName = usePathname();
+  const pagePathName = pathName.split("/")[2];
+
+  useEffect(() => {
+    const searchAction = setTimeout(() => {
+      if (search && genre) {
+        router.push(`?search=${search}&genre=${genre}`);
+      } else if (search) {
+        router.push(`?search=${search}`);
+      } else if (genre) {
+        router.push(`?genre=${genre}`);
+      } else {
+        router.push(`/search/${pagePathName}`);
+      }
+    }, 500);
+
+    return () => clearTimeout(searchAction);
+  }, [search, genre, router, pagePathName]);
+
+  useEffect(() => {
+    setSearch("");
+    setGenre("");
+  }, [pathName]);
+
   return (
     <div className="flex w-full items-center justify-center">
       <div className="relative">
@@ -14,9 +46,11 @@ const InputSearch = () => {
         <Input
           placeholder="Search..."
           className="w-[150px] px-6 placeholder:italic sm:w-[250px] md:w-[300px]"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
         />
       </div>
-      <SelectGenres />
+      <SelectGenre setSelectValue={setGenre} />
     </div>
   );
 };
