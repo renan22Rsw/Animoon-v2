0d2697fdbe1ec24e559b74db85475daefa27528f
Renan Victor <renan22rsw@gmail.com>
2025-02-14 20:32:02 -0300

    feat: profile info component and actions for getting datas length

--- src/app/user/profile/page.tsx
+++ src/app/user/profile/page.tsx
@@ -1,10 +1,16 @@
 import { redirect } from "next/navigation";
 import { auth } from "../../../../auth";
 import UserProfileHeader from "./_components/profile-header";
+import ProfileInfo from "./_components/profile-info";
+import { getAnimesLength } from "@/data/animoon/animes/get-animes-length";
+import { getMangasLength } from "@/data/animoon/mangas/get-mangas-length";
+import { getFavoritesLength } from "@/data/animoon/get-favorites-length";
 
 const UserProfilePage = async () => {
   const session = await auth();
-
+  const animesLenght = await getAnimesLength();
+  const mangasLenght = await getMangasLength();
+  const favoritesLenght = await getFavoritesLength();
   if (!session?.user) {
     redirect("/login");
   }
@@ -14,6 +20,11 @@ const UserProfilePage = async () => {
       <UserProfileHeader
         name={session.user.name as string}
         img={session.user.image as string}
+      />
+      <ProfileInfo
+        animesLenght={animesLenght as number}
+        mangasLenght={mangasLenght as number}
+        favoritesLenght={favoritesLenght as number}
       />
     </>
   );
---
+++ src/data/animoon/mangas/get-mangas-length.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import { prisma as db } from "@/database/db";
+import { auth } from "../../../../auth";
+
+export const getMangasLength = async () => {
+  const session = await auth();
+  if (!session?.user?.id) {
+    return 0;
+  }
+  try {
+    const mangasLenght = await db.mangaList.count({
+      where: {
+        userId: session.user.id,
+      },
+    });
+
+    return mangasLenght;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
